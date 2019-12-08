96460da7ad84819b840159409d224599b8fc4f07
hasherezade <hasherezade@gmail.com>
2019-12-08 19:53:09 +0100

    [FEATURE] Added beardisasm.h

---
+++ disasm/include/beardisasm.h
@@ -0,0 +1,19 @@
+#ifndef BEARDISASM_H
+#define BEARDISASM_H
+
+#include "AddrConverter.h"
+#include "CodeBlock.h"
+#include "Context.h"
+#include "DisasmBase.h"
+#include "DisasmChunk.h"
+#include "DisasmChunkBuf.h"
+#include "Tracer.h"
+
+#include "dos/DosDisasm.h"
+#include "dos/DosTracer.h"
+
+#include "pe/PeDataFetcher.h"
+#include "pe/PeDisasm.h"
+#include "pe/PeTracer.h"
+
+#endif //BEARDISASM_H
